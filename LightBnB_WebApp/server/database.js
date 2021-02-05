9b155c334dbb170a8b1f15ffd33bacd111d65de3
Perry Defayette <defayetteperry@gmail.com>
2021-02-05 01:29:51 +0000

    Add options.

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -137,15 +137,60 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  const queryString = `
-    SELECT * 
-    FROM properties
-    LIMIT $1;
-  `;
-  const values = [limit];
+  console.log('limit', limit, 'options:', options, 'number of options: ', Object.values(options).length);
 
-  return pool.query(queryString, values)
-    .then(res => res.rows)
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  WHERE true
+  `;
+
+  // 3
+  //handle city option
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `AND city LIKE $${queryParams.length} `;
+  }
+  if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`);
+    queryString += `AND owner_id = $${queryParams.length} `;
+  }
+  //handle minimum price
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    //multiply price by 100 since db deals with price in pennies
+    queryParams.push(`${options.minimum_price_per_night * 100}`);
+    queryParams.push(`${options.maximum_price_per_night * 100}`);
+    queryString += `AND (cost_per_night >= $${queryParams.length - 1} AND cost_per_night <= $${queryParams.length}) `;
+  }
+
+  //handle minimum rating
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += `AND property_reviews.rating >= $${queryParams.length} `;
+  }
+
+  // 4
+  queryParams.push(limit);
+  
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  // 5
+  console.log('queryString: ', queryString, 'queryParams:', queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams)
+    .then(res => {
+      // console.log(res.rows);
+      return res.rows;
+    })
     .catch(err => console.error('query error', err.stack));
 };
 exports.getAllProperties = getAllProperties;
